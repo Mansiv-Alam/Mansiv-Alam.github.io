2bf1cf546b5c9a137e996e6467c8d3e741039e69
Mansiv Alam <mansiv.alam1@gmail.com>
2026-06-30 22:44:00 -0400

    Improve page layout styling

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const track = document.querySelector(".track");
 const dots = document.querySelectorAll(".dot");
+const slideWidth = document.querySelector(".slider").clientWidth;
 
 let current = 0;
 
 function showSlide(index) {
     current = index;
 
-    track.style.transform = `translateX(-${700 * current}px)`;
+    track.style.transform = `translateX(-${slideWidth * current}px)`;
 
     dots.forEach(dot => dot.classList.remove("active"));
     dots[current].classList.add("active");
